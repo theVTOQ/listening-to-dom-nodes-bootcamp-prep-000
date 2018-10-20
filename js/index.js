8f21f585aa493b5341a3aeefbcb5ed7cdedc253c
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-20 22:18:58 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -7,14 +7,14 @@ main.addEventListener('click', function (event) {
 });
 
 const divs = document.querySelectorAll('div');
- 
+
 function bubble(e) {
   // stop! that! propagation!
   e.stopPropagation();
- 
+
   console.log(this.firstChild.nodeValue.trim() + ' bubbled');
 }
- 
+
 for (let i = 0; i < divs.length; i++) {
   divs[i].addEventListener('click', bubble);
 }

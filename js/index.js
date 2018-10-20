7fb3316a07890076aa1be618e7b81f645a12b93a
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-20 22:19:25 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,20 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const main = document.getElementById('main');
+
+main.addEventListener('click', function (event) {
+  alert("I was clicked!");
+});
+
+const divs = document.querySelectorAll('div');
+
+function bubble(e) {
+  // stop! that! propagation!
+  e.stopPropagation();
+
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
